c0a308d9ac58a0f5b45a407a94e15996d55a1355
J2021T <80201567+J2021T@users.noreply.github.com>
2021-05-08 05:27:20 -0400

    started html set up, css styling, and js questions

---
+++ script.js
@@ -0,0 +1,55 @@
+var highScore = 0;
+var score = //timer value;
+var questionsArr = [
+    {
+        question: "Which is NOT a JavaScript date type?",
+        answers: {
+            a: 'boolean',
+            b: 'string',
+            c: 'infinity',
+            d: 'number'
+        },
+        correctAns: 'c'
+    },
+    {
+        question: 'An undefined variable is a variable that has been given a value.',
+        answers: {
+            a: 'True',
+            b: 'False'
+        },
+        correctAns: 'b'
+    },
+    {
+        question: 'Which symbol is used for single line comments in JavaScript?',
+        answer: {
+            a: '//',
+            b: '+',
+            c: "/*",
+            d: '$'
+        },
+        correctAns: 'a'
+    },
+    {
+        question: 'Which is not a looping structure in JavaScript?',
+        answers: {
+            a: 'For',
+            b: 'While',
+            c: 'Do-While',
+            d: 'If'
+        },
+        correctAns: 'd'
+    },
+    {
+        question: 'Which is one of the types of Pop up boxes available in JavaScript?',
+        answers: {
+            a: 'Window',
+            b: 'Alert',
+            c: 'Id-Box',
+            d: 'Pop'
+        },
+        correctAns: 'b'
+    },
+    {
+        question: ''
+    }
+];
